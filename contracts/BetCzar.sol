f1abdaff6a24677be3e879dfd2459bb2152b1dea
Dmitriy Vasilyuk <irreverenceofreason@gmail.com>
2022-11-10 06:41:14 +0300

    add events

--- contracts/BetCzar.sol
+++ contracts/BetCzar.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
-0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db,0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB,1000000000000000000,2000000000000000000
+// 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db,0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB,1000000000000000000,2000000000000000000
 contract BetCzar {
     //TODO emit events
     enum BetStatus {
@@ -16,6 +16,18 @@ contract BetCzar {
         uint amt1;
         uint amt2;
     }
+    event BetCreated(
+        uint betId,
+        address indexed bettor1, 
+        address indexed bettor2,
+        address indexed judge,
+        uint amt1,
+        uint amt2
+    );
+    event BetStatusChange(
+        uint indexed betId,
+        BetStatus newStatus
+    );
     error InvalidId(); //cheaper than string 
     error InvalidStatus();
 
@@ -43,6 +55,7 @@ contract BetCzar {
         betId = bets.length;
         bets.push(Bet(_bettor1, _bettor2, _judge, BetStatus.CREATED, 
             _amt1, _amt2));
+        emit BetCreated(betId, _bettor1, _bettor2, _judge, _amt1, _amt2);
     }
     function deposit1(uint betId) external payable {
         //deposit for bettor1
@@ -50,8 +63,10 @@ contract BetCzar {
         require(msg.value == bet.amt1);
         if (bet.status == BetStatus.CREATED) {
             bets[betId].status = BetStatus.WAITING_FOR2;
+            emit BetStatusChange(betId, BetStatus.WAITING_FOR2);
         } else if (bet.status == BetStatus.WAITING_FOR1) {
             bets[betId].status = BetStatus.PENDING;
+            emit BetStatusChange(betId, BetStatus.PENDING);
         } else {
             revert InvalidStatus();
         }
@@ -63,8 +78,10 @@ contract BetCzar {
         require(msg.value == bet.amt2);
         if (bet.status == BetStatus.CREATED) {
             bets[betId].status = BetStatus.WAITING_FOR1;
+            emit BetStatusChange(betId, BetStatus.WAITING_FOR1);
         } else if (bet.status == BetStatus.WAITING_FOR2) {
             bets[betId].status = BetStatus.PENDING;
+            emit BetStatusChange(betId, BetStatus.PENDING);
         } else {
             revert InvalidStatus();
         }
@@ -93,8 +110,9 @@ contract BetCzar {
         //first change status 
         bets[betId].status = BetStatus.CREATED;
         //now refund the bettor
-        (bool success, bytes memory data) = msg.sender.call{value: amt}("");
+        (bool success, ) = msg.sender.call{value: amt}("");
         require(success, "send failed");
+        emit BetStatusChange(betId, BetStatus.CREATED);
     }
 /*
     function recall2(uint betId) external {
@@ -117,10 +135,13 @@ contract BetCzar {
         require(bet.status == BetStatus.PENDING, "not pending");
         if (winner == 1) {
             bets[betId].status = BetStatus.WON1;
+            emit BetStatusChange(betId, BetStatus.WON1);
         } else if (winner == 2) {
             bets[betId].status = BetStatus.WON2;
+            emit BetStatusChange(betId, BetStatus.WON2);
         } else {
             bets[betId].status = BetStatus.CANCELED;
+            emit BetStatusChange(betId, BetStatus.CANCELED);
         }
     }
 
@@ -131,8 +152,10 @@ contract BetCzar {
         require(bet.status == BetStatus.PENDING, "not pending");
         if (msg.sender == bet.bettor1) {
             bets[betId].status = BetStatus.WON2;
+            emit BetStatusChange(betId, BetStatus.WON2);
         } else if (msg.sender == bet.bettor2) {
             bets[betId].status = BetStatus.WON1;
+            emit BetStatusChange(betId, BetStatus.WON1);
         } else {
             revert("not a bettor"); //optimize gas by not reverting?
         }
@@ -143,15 +166,17 @@ contract BetCzar {
         Bet memory bet = getBet(betId);
         if (bet.status == BetStatus.WON1) {
             bets[betId].status = BetStatus.CLAIMED1;
-            (bool success, bytes memory data) = 
+            (bool success, ) = 
                 bet.bettor1.call{value: bet.amt1 + bet.amt2}("");
             require(success, "send failed");
+            emit BetStatusChange(betId, BetStatus.CLAIMED1);
 
         } else if (bet.status == BetStatus.WON2) {
             bets[betId].status = BetStatus.CLAIMED2;
-            (bool success, bytes memory data) = 
+            (bool success, ) = 
                 bet.bettor2.call{value: bet.amt1 + bet.amt2}("");
             require(success, "send failed");
+            emit BetStatusChange(betId, BetStatus.CLAIMED2);
 
         } else if (bet.status == BetStatus.CANCELED) {
             //refund bettors their amounts
@@ -159,42 +184,53 @@ contract BetCzar {
             //refund to the other and set a special status
             //and of course guard against reentrancy
             bets[betId].status = BetStatus.CLAIMED_REFUND1;
-            (bool success, bytes memory data) = 
+            (bool success, ) = 
                 bet.bettor1.call{value: bet.amt1}("");
 
             //This part is slightly tricky because success could be true or false.
             //we now make sure the correct current status is stored in
             //the memory variable bet.status, and set the status in storage 
+            //(as well as the variable hopefullyFinalStatus - to avoid an extra storage read)
             //to what it should be if the next transfer - refund to bettor 2 - 
             //succeeds. 
+            //We could have done this in a much simpler way if we didn't need to 
+            //guard against reentrancy
+            BetStatus hopefullyFinalStatus;
             if (success) {
                 bet.status = BetStatus.CLAIMED_REFUND1;
+                hopefullyFinalStatus = BetStatus.CLAIMED_REFUNDS; 
                 bets[betId].status = BetStatus.CLAIMED_REFUNDS;
             } else {
-                //revert status change (have to do it this way - reentrancy!
+                hopefullyFinalStatus = BetStatus.CLAIMED_REFUND2;
                 bets[betId].status = BetStatus.CLAIMED_REFUND2;
             }
             //now try to refund the second bettor
-            (success, data) = 
+            //at this point bet.status is the current status and hopefullyFutureStatus is
+            //the status stored in storage, aka correct status if the following transfer succeeds
+            (success, ) = 
                 bet.bettor2.call{value: bet.amt2}("");
-            if (!success) {
-                //revert status change (have to do it this way - reentrancy!
+            if (success) {
+                emit BetStatusChange(betId, hopefullyFinalStatus);
+            } else {
                 bets[betId].status = bet.status;
+                emit BetStatusChange(betId, bet.status);
             }
 
         } else if (bet.status == BetStatus.CLAIMED_REFUND1) {
             // refund bettor 2 only
             bets[betId].status = BetStatus.CLAIMED_REFUNDS;
-            (bool success, bytes memory data) = 
+            (bool success, ) = 
                 bet.bettor2.call{value: bet.amt2}("");
             require(success, "send failed");
+            emit BetStatusChange(betId, BetStatus.CLAIMED_REFUNDS);
 
         } else if (bet.status == BetStatus.CLAIMED_REFUND2) {
             // refund bettor 1 only
             bets[betId].status = BetStatus.CLAIMED_REFUNDS;
-            (bool success, bytes memory data) = 
+            (bool success, ) = 
                 bet.bettor1.call{value: bet.amt1}("");
             require(success, "send failed");
+            emit BetStatusChange(betId, BetStatus.CLAIMED_REFUNDS);
 
         } else {
             revert InvalidStatus();
