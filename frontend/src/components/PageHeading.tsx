8c75ff451e1e92ebc24282ea11ed41b318731d59
Dmitriy Vasilyuk <irreverenceofreason@gmail.com>
2022-12-06 09:28:45 +0300

    remove frontend since it's been split off

--- frontend/src/components/PageHeading.tsx
+++
@@ -1,9 +0,0 @@
-import Typography from "@mui/material/Typography";
-
-export const PageHeading = ({ text }: { text: string }) => {
-  return (
-    <Typography variant="h5" textAlign="center" gutterBottom paragraph color="primary.main">
-      {text}
-    </Typography>
-  );
-};
